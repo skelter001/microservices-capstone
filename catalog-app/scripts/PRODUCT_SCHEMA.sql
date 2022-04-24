5947cedca6466be92a24216c76d57b6acdb37d7c
xaghoul <ggamzaty@gmail.com>
2022-04-24 13:03:43 +0300

    feat: Update product table schema

--- catalog-app/scripts/PRODUCT_SCHEMA.sql
+++ catalog-app/scripts/PRODUCT_SCHEMA.sql
@@ -1,4 +1,4 @@
-CREATE TABLE product (
+CREATE TABLE IF NOT EXISTS product (
     uniq_id VARCHAR(100) NOT NULL UNIQUE,
     sku VARCHAR(100),
     name_title VARCHAR(255),
@@ -7,6 +7,7 @@ CREATE TABLE product (
     sale_price VARCHAR(40),
     category VARCHAR(50),
     category_tree VARCHAR(255),
-    average_product_price VARCHAR(30),
-    product_url VARCHAR(100)
+    average_product_rating VARCHAR(30),
+    product_url VARCHAR(100),
+    brand VARCHAR(100)
 )
