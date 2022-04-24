0919c78c721bc3b4f28a551997393a23162d0aa6
xaghoul <ggamzaty@gmail.com>
2022-04-24 13:08:27 +0300

    feat: Add logging to InventoryController

--- inventory-app/src/main/java/com/xaghoul/inventoryapp/controller/InventoryController.java
+++ inventory-app/src/main/java/com/xaghoul/inventoryapp/controller/InventoryController.java
@@ -2,6 +2,7 @@ package com.xaghoul.inventoryapp.controller;
 
 import com.xaghoul.inventoryapp.service.InventoryService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +12,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/inventory")
 @AllArgsConstructor
+@Slf4j
 public class InventoryController {
 
-    private InventoryService inventoryService;
+    private final InventoryService inventoryService;
 
     @GetMapping
     private List<String> getAllAvailableProductIds() {
+        log.info("Get all available products");
         return inventoryService.getAllAvailableProductIds();
     }
 }
