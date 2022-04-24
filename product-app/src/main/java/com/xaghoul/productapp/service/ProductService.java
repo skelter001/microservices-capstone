901391abe6459a105717694a1ab74af81653ffda
xaghoul <ggamzaty@gmail.com>
2022-04-24 15:46:59 +0300

    feat: Add logging to ProductService

--- product-app/src/main/java/com/xaghoul/productapp/service/ProductService.java
+++ product-app/src/main/java/com/xaghoul/productapp/service/ProductService.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -42,5 +43,9 @@ public class ProductService {
         log.debug("Product Service: got list of products {}", productDTOS);
 
         return productDTOS;
+    }
+
+    public List<ProductDTO> getProductsBySkuCatalog(String sku) {
+        return new ArrayList<>(catalogClient.getProductsBySku(sku));
     }
 }
