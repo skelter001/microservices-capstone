724ac4c6a9aca1687571bc35f9a440ac3b08d3ac
xaghoul <ggamzaty@gmail.com>
2022-04-25 20:14:46 +0300

    feat: Implement Circuit breaker to product service

--- product-app/src/main/java/com/xaghoul/productapp/client/CatalogClient.java
+++ product-app/src/main/java/com/xaghoul/productapp/client/CatalogClient.java
@@ -1,6 +1,7 @@
 package com.xaghoul.productapp.client;
 
 import com.xaghoul.common.dto.ProductDTO;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +12,18 @@ import java.util.List;
 public interface CatalogClient {
 
     @GetMapping("/id/{uniqId}")
+    @CircuitBreaker(name = "catalogService", fallbackMethod = "getProductByIdFallback")
     ProductDTO getProductById(@PathVariable String uniqId);
 
     @GetMapping("/sku/{sku}")
+    @CircuitBreaker(name = "catalogService", fallbackMethod = "getProductsBySkuFallback")
     List<ProductDTO> getProductsBySku(@PathVariable String sku);
+
+    default ProductDTO getProductByIdFallback(Exception ex) {
+        return ProductDTO.builder().build();
+    }
+
+    default List<ProductDTO> getProductsBySkuFallback(Exception ex) {
+        return List.of();
+    }
 }
--- product-app/src/main/java/com/xaghoul/productapp/client/InventoryClient.java
+++ product-app/src/main/java/com/xaghoul/productapp/client/InventoryClient.java
@@ -1,5 +1,6 @@
 package com.xaghoul.productapp.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,5 +10,10 @@ import java.util.List;
 public interface InventoryClient {
 
     @GetMapping
+    @CircuitBreaker(name = "inventoryService", fallbackMethod = "getAllAvailableProductIdsFallback")
     List<String> getAllAvailableProductIds();
+
+    default List<String> getAllAvailableProductIdsFallback(Exception ex) {
+        return List.of();
+    }
 }
