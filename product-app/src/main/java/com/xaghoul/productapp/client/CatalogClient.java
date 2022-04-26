3d75ad7ca9d447236f57fe50043492789cc9a3a6
xaghoul <ggamzaty@gmail.com>
2022-04-26 17:09:35 +0300

    fix: Remove exceed profile

--- product-app/src/main/java/com/xaghoul/productapp/client/CatalogClient.java
+++ product-app/src/main/java/com/xaghoul/productapp/client/CatalogClient.java
@@ -1,7 +1,6 @@
 package com.xaghoul.productapp.client;
 
 import com.xaghoul.common.dto.ProductDTO;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- product-app/src/main/java/com/xaghoul/productapp/client/InventoryClient.java
+++ product-app/src/main/java/com/xaghoul/productapp/client/InventoryClient.java
@@ -1,6 +1,5 @@
 package com.xaghoul.productapp.client;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
