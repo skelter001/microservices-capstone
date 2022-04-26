fab8d2aba60ef34b5e350f799518705caa54f84b
xaghoul <ggamzaty@gmail.com>
2022-04-26 10:54:55 +0300

    fix: Update table schema

--- catalog-app/scripts/PRODUCT_SCHEMA.sql
+++ catalog-app/scripts/PRODUCT_SCHEMA.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS product (
     uniq_id VARCHAR(100) NOT NULL UNIQUE,
     sku VARCHAR(100),
-    name_title VARCHAR(255),
-    description VARCHAR(255),
-    list_price VARCHAR(40),
-    sale_price VARCHAR(40),
-    category VARCHAR(50),
-    category_tree VARCHAR(255),
+    name_title TEXT,
+    description TEXT,
+    list_price VARCHAR(20),
+    sale_price VARCHAR(20),
+    category TEXT,
+    category_tree TEXT,
     average_product_rating VARCHAR(30),
-    product_url VARCHAR(100),
+    product_url TEXT,
     brand VARCHAR(100)
 )
